277f7441636c44d40a720a6c8c091a941893f792
Zhenquan Ma <shinnmaa@gmail.com>
2019-04-24 16:28:51 -0400

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/zhenquan/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package zhenquan.springframework.petclinic.services;
+
+import zhenquan.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
