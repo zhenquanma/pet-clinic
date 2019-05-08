d05abd37221a2fe1f95dceecc572998aff8d8721
Zhenquan Ma <shinnmaa@gmail.com>
2019-05-08 19:28:59 -0400

    Implement Map Impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/zhenquan/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package zhenquan.springframework.petclinic.services;
+
+import zhenquan.springframework.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/zhenquan/springframework/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package zhenquan.springframework.petclinic.services.map;
+
+import zhenquan.springframework.petclinic.model.Visit;
+import zhenquan.springframework.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstactMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(object == null || object.getPet() == null || object.getPet().getId() == null ||
+
+                object.getPet().getOwner() == null || object.getPet().getOwner().getId() == null){
+
+            throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
