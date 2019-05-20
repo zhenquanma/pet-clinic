ac8e8ea2701969e20f8323f016a14c6decd88df4
Zhenquan Ma <shinnmaa@gmail.com>
2019-05-19 22:45:35 -0400

    Added ability to add, update a pet. Close #56

---
+++ pet-clinic-web/src/test/java/zhenquan/springframework/petclinic/controllers/PetControllerTest.java
@@ -0,0 +1,104 @@
+package zhenquan.springframework.petclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import zhenquan.springframework.petclinic.model.Owner;
+import zhenquan.springframework.petclinic.model.Pet;
+import zhenquan.springframework.petclinic.model.PetType;
+import zhenquan.springframework.petclinic.services.OwnerService;
+import zhenquan.springframework.petclinic.services.PetService;
+import zhenquan.springframework.petclinic.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class PetControllerTest {
+    @Mock
+    PetService petService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @Mock
+    OwnerService ownerService;
+
+    PetController petController;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypeSet;
+    Pet pet;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        owner = Owner.builder().id(1L).build();
+        petTypeSet = new HashSet<>();
+        petTypeSet.add(PetType.builder().id(1L).name("Dog").build());
+        petTypeSet.add(PetType.builder().id(2L).name("Cat").build());
+        pet = Pet.builder().id(1L).build();
+
+        petController = new PetController(petService, ownerService, petTypeService);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(petController).build();
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypeSet);
+
+        mockMvc.perform(get("/owners/1/pets/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    void processCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypeSet);
+
+        mockMvc.perform(post("/owners/1/pets/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+    }
+
+    @Test
+    void initUpdateForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypeSet);
+        when(petService.findById(anyLong())).thenReturn(pet);
+
+        mockMvc.perform(get("/owners/1/pets/1/edit"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("pets/createOrUpdatePetForm"))
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"));
+    }
+
+    @Test
+    void processUpdateForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypeSet);
+
+        mockMvc.perform(post("/owners/1/pets/1/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+    }
+}
