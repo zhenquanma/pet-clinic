b3b267b5a19dabe68d6fda57b5e3224b2ff5d130
Zhenquan Ma <shinnmaa@gmail.com>
2019-05-23 00:02:06 -0400

    Added json endpoint. Closes #64

--- pet-clinic-web/src/main/java/zhenquan/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/zhenquan/springframework/petclinic/controllers/VetController.java
@@ -2,8 +2,12 @@ package zhenquan.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import zhenquan.springframework.petclinic.model.Vet;
 import zhenquan.springframework.petclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -15,9 +19,15 @@ public class VetController {
     }
 
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
+
 }
